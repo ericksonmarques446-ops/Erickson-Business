f10c9acc2b25f546b7086be39b55d6291ade5707
ericksonmarques446-ops <ericksonmarques446@gmail.com>
2026-09-13 03:50:33 +0100

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu");
 
-    if (menu) {
-        menu.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+
+    if (nav.style.display === "flex") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "flex";
+        nav.style.flexDirection = "column";
+        nav.style.position = "absolute";
+        nav.style.top = "80px";
+        nav.style.left = "0";
+        nav.style.width = "100%";
+        nav.style.padding = "25px";
+        nav.style.background = "#0A0A0A";
     }
-}
-
-function getToken() {
-    return localStorage.getItem("eb_token");
-}
-
-function logout() {
-    localStorage.removeItem("eb_token");
-    window.location.href = "index.html";
 }
